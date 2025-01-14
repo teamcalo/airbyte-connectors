9f46366bd53b19315653ddf603f9182b8e4662a1
Bohdan <bohdan@calo.app>
2025-01-14 17:27:46 +0300

    Refactor AWS Amplify source connector to support multiple authentication methods and remove deprecated config file

--- connectors/aws-amplify-source/source_aws_amplify/components.py
+++ connectors/aws-amplify-source/source_aws_amplify/components.py
@@ -1,46 +1,77 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, InitVar
+from abc import ABC, abstractmethod
 
 import boto3
 import requests
 from airbyte_cdk.sources.declarative.auth.declarative_authenticator import DeclarativeAuthenticator
+from airbyte_cdk.sources.declarative.interpolation import InterpolatedString
 from airbyte_cdk.sources.declarative.types import Config
 from botocore.auth import SigV4Auth
 from botocore.awsrequest import AWSRequest
+from typing import Union, Mapping, Any
 
 
 @dataclass
-class IAMAuthenticator(DeclarativeAuthenticator):
+class BaseIAMAuthenticator(DeclarativeAuthenticator, ABC):
     config: Config
+    parameters: InitVar[Mapping[str, Any]]
+
+    @abstractmethod
+    def sign(self):
+        pass
 
     def __call__(self, request: requests.PreparedRequest):
         region = self.config.get("region", "us-east-1")
-        access_key_id = self.config.get("access_key_id", None)
-        secret_access_key = self.config.get("secret_access_key", None)
-        assume_role = self.config.get("assume_role", None)
 
-        credentials = None
-        if assume_role:
-            sts_client = boto3.client(
-                "sts",
-                region_name=region,
-                aws_access_key_id=access_key_id,
-                aws_secret_access_key=secret_access_key,
-            )
-            credentials = sts_client.assume_role(
-                RoleArn=assume_role, RoleSessionName="CrossAcctSession"
-            )["Credentials"]
-        else:
-            session = boto3.Session(
-                region_name=region,
-                aws_access_key_id=access_key_id,
-                aws_secret_access_key=secret_access_key,
-                
-            )
-            credentials = session.get_credentials()
-        
         aws_req = AWSRequest(method=request.method, url=request.url, headers={"Host": f"amplify.{region}.amazonaws.com"})
-
-        SigV4Auth(credentials, "amplify", region).add_auth(aws_req)
+        SigV4Auth(self.sign(), "amplify", region).add_auth(aws_req)
 
         request.headers.update(dict(aws_req.headers))
         return request
+
+
+@dataclass
+class IAMCredentialsAuthenticator(BaseIAMAuthenticator):
+    access_key_id: Union[InterpolatedString, str]
+    secret_access_key: Union[InterpolatedString, str]
+
+    def sign(self):
+        region = self.config.get("region", "us-east-1")
+
+        session = boto3.Session(
+            region_name=region,
+            aws_access_key_id=str(self.access_key_id.eval(self.config)),
+            aws_secret_access_key=str(self.secret_access_key.eval(self.config)),
+        )
+        return session.get_credentials()
+
+    def __post_init__(self, parameters: Mapping[str, Any]):
+        if isinstance(self.access_key_id, str):
+            self.access_key_id = InterpolatedString(self.access_key_id, parameters=parameters)
+        if isinstance(self.secret_access_key, str):
+            self.secret_access_key = InterpolatedString(self.secret_access_key, parameters=parameters)
+
+
+@dataclass
+class IAMRoleAuthenticator(BaseIAMAuthenticator):
+    assume_role: Union[InterpolatedString, str]
+
+    def sign(self):
+        region = self.config.get("region", "us-east-1")
+        sts_client = boto3.client("sts", region_name=region)
+        response = sts_client.assume_role(
+            RoleArn=str(self.assume_role.eval(self.config)), 
+            RoleSessionName="CrossAcctSession"
+        )
+        credentials = response['Credentials']
+
+        session = boto3.Session(
+            aws_access_key_id=credentials['AccessKeyId'],
+            aws_secret_access_key=credentials['SecretAccessKey'],
+            aws_session_token=credentials['SessionToken'],
+        )
+        return session.get_credentials()
+
+    def __post_init__(self, parameters: Mapping[str, Any]):
+        if isinstance(self.assume_role, str):
+            self.assume_role = InterpolatedString(self.assume_role, parameters=parameters)
